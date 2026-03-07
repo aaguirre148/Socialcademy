6ae2cbe87effaae89cef37aed2803123ec571915
adriano <71270161+aaguirre148@users.noreply.github.com>
2026-03-06 22:07:12 -0600

    Initial Commit

---
+++ Socialcademy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Socialcademy
+//
+//  Created by Adrian Aguirre on 06/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Adrian Aguirre on 06/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
